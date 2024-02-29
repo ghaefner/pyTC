87c17e032bbf382d13fb08753328d7df2ac48436
ghaefner <guillaume-haefner@gmx.de>
2024-02-29 17:04:13 +0100

    Fix liquid data (ld) date parser

--- pytc/src/reader.py
+++ pytc/src/reader.py
@@ -1,6 +1,9 @@
 from datetime import datetime
-from pandas import read_excel
+import pandas as pd
+from locale import setlocale, LC_ALL
+import re
 
+from config import Columns
 
 class DataReader:
     def __init__(self, data_source):
@@ -12,6 +15,25 @@ class DataReader:
             return self.read_csv()
         
     def read_unify_excel(self):
-        df = read_excel()
+        df = pd.read_excel("your_file_path.xlsx")
+
+        df[Columns.DATE] = df["Time"].apply(parse_ld_date)
+
+        # df["Time"].apply(lambda x: datetime.strptime(x.split("[")[0].strip(), "%b, %y"))
+
+        # df = df.pivot_table(index=["date", "Geography", "Product", "MARKE"], columns="Metric", values="Verkauf 1.000 Euro").reset_index()
+
+        # Rename the columns
+        # df.columns = ["date", "region", "market", "product", "metric", "value"]
+
+        return df
         
-    
+
+def parse_ld_date(date, locale='de_DE'):
+    setlocale(LC_ALL, locale=locale)
+    date = re.sub(r"\[W \d\d-\d\d]", "", date)
+    date = re.sub(r"MAT |YTD ", "", date)
+    date = date.replace("Mär", "Mrz")
+    date = date.replace(",","")
+    date = datetime.strptime(date, "%b %y")
+    return date
