b91caf686363d00cd74d9c82098c0ac414877479
ghaefner <guillaume-haefner@gmx.de>
2024-02-29 17:25:06 +0100

    Add Column Map class to config file

--- pytc/config.py
+++ pytc/config.py
@@ -6,3 +6,13 @@ class Columns:
     DATE = "date"
     METRIC = "metric"
     VALUE = "value"
+
+
+class ColumnMap:
+
+    LD = {
+    "Geography": Columns.REGION,
+    "Product": Columns.MARKET,
+    "MARKE": Columns.PRODUCT,
+    "Verkauf 1.000 Euro": Columns.VALUE
+}
