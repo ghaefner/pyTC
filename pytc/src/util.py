9e644668ffe5c7ae3c43eaa50700f88b0f65c7c0
ghaefner <guillaume-haefner@gmx.de>
2024-03-01 10:21:42 +0100

    Add utility functions for later

---
+++ pytc/src/util.py
@@ -0,0 +1,27 @@
+import os
+import re
+ 
+def skip_rows(df, n_rows):
+    return df.iloc[n_rows:, :].reset_index(drop=True)
+ 
+def skip_incomplete_rows(df):
+    n_row = first_complete_row(df)
+    return skip_rows(df, n_row).infer_objects() if n_row else df
+ 
+def first_complete_row(df):
+    for i in range(len(df)):
+        if not df.iloc[i].isnull().values.any():
+            return i
+    return False
+ 
+def use_first_row_as_header(df):
+    return df.rename(columns=df.iloc[0]).drop(df.index[0]).reset_index(drop=True).infer_objects()
+ 
+def list_files_by_pattern(path, pattern):
+    return [os.path.join(path, f) for f in os.listdir(path) if re.search(pattern, f)]
+ 
+def drop_zero(df):
+    return df.loc[~(df == 0).all(axis=1)]
+ 
+def list_intersection(list1, list2):
+    return [value for value in list1 if value in list2]
