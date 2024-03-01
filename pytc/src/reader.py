0b082655e0eac1433d8aad0901d463927984d74d
ghaefner <guillaume-haefner@gmx.de>
2024-03-01 11:12:55 +0100

    Add UnifyReader from Reader ABC

--- pytc/src/reader.py
+++ pytc/src/reader.py
@@ -1,31 +1,33 @@
 from datetime import datetime
 import pandas as pd
 from locale import setlocale, LC_ALL
+from abc import ABC, abstractmethod
 
-from config import Columns, ColumnMap, PATH_TO_DATA
+from config import Columns, ColumnMap, Config
 
-class DataReader:
-    def __init__(self, data_source):
-        self.data_source = data_source
-        self.path = PATH_TO_DATA
-        
+class Reader(ABC):
+    @abstractmethod
+    def read(self):
+        yield None
+
+class UnifyReader(Reader):
+    def __init__(self, config=Config):
+        self.config = config
+        self.file_path = self.config.PATH_TO_DATA
     
-    def read_data(self):
-        if self.data_source == "unify" or self.data_source == "ld":
-            return self.read_unify_excel(self)
-        
-    def read_unify_excel(self):
-        df = pd.read_excel(self.path)
+    def read(self):
+        df = pd.read_excel(self.file_path)
 
-        df[Columns.DATE] = df["Time"].apply(parse_ld_date)
+        df[Columns.DATE] = df["Time"].apply(self.parse_ld_date)
         df.rename(columns=ColumnMap.LD, inplace=True)
         df[Columns.METRIC] = "KEUR"
     
         df.drop(columns=set(df.columns) - set(vars(Columns).values()), inplace=True, errors="ignore")
-        self.data = df
+        
+        yield df
 
-def parse_ld_date(date_str):
-    setlocale(LC_ALL, "de_DE")
-    date_str = date_str.split("[")[0].strip()
-    date_str = date_str.replace("Mär", "Mrz")
-    return datetime.strptime(date_str, "%b, %y")
+    def parse_ld_date(date_str):
+        setlocale(LC_ALL, "de_DE")
+        date_str = date_str.split("[")[0].strip()
+        date_str = date_str.replace("Mär", "Mrz")
+        return datetime.strptime(date_str, "%b, %y")
