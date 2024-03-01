498f93eb9066b528d8ba9cbf8f771b6ee6f82127
ghaefner <guillaume-haefner@gmx.de>
2024-03-01 11:15:24 +0100

    Add Task class

--- pytc/src/model.py
+++ pytc/src/model.py
@@ -1,27 +1,18 @@
-import datetime as dt
-from config import Columns
+from dataclasses import dataclass
+from reader import Reader
+from typing import Callable, List
 
+@dataclass
+class Task:
+    source: str
+    func: Callable
+    table: str
+
+
+@dataclass
 class Model:
-    def __init__(self, market, product, region, date, metric, value):
-        
-        self._validate_data_type({
-            Columns.MARKET: (market, str),            
-            Columns.PRODUCT: (product, str),
-            Columns.REGION: (region, str),
-            Columns.DATE: (date, dt.date),
-            Columns.METRIC: (metric, str),
-            Columns.VALUE: (value, float)
-        })
-        
-        self.market = market
-        self.product = product
-        self.region = region
-        self.date = date
-        self.metric = metric
-        self.value = value
-        
-    def _validate_data_types(self, data_types):
-        for attr_name, (attr_value, expected_type) in data_types.items():
-            if not isinstance(attr_value, expected_type):
-                raise TypeError(f"{attr_name.capitalize()} must be of type {expected_type.__name__}.")
-        
+    name: str
+    name_long: str
+    reader: Reader
+    task: List[Task]
+    column_map: dict = None
