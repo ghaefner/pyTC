15d64bb2adbf8302f4c1066f34378b142ada0267
ghaefner <guillaume-haefner@gmx.de>
2024-03-04 16:30:23 +0100

    Add writer class with csvwriter

---
+++ pytc/src/writer.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from time import perf_counter
+import logging as log
+import os
+import pandas as pd
+
+class Writer(ABC):
+    @abstractmethod
+    def write(data):
+        return None
+    
+
+class CsvWriter(Writer):
+    def __init__(self, file_path, mode="a"):
+        self.file_path = file_path
+        self.counter = 0
+        self.mode = mode
+
+    def write(
+            self,
+            data_df,
+            file_name
+    ):
+        start = perf_counter()
+        log.info(f"[I] Writing {data_df.shape[0]}")
+        log.info(f"[I] Columns: {data_df.columns.tolist()}")
+        log.info(f"\n{data_df.head(1)}")
+
+        if self.mode == "a":
+            path = os.path.join(self.file_path, f"{file_name}.csv")
+            data_df.sort_index(axis=1).to_csv(
+                path, index=False, mode="a", header=not os.path.isfile(path)
+            )
+        elif self.mode == "w":
+            path = os.path.join(self.file_path, f"{file_name}.csv")
+            self.counter += 1
+            data_df.sort_index(axis=1).to_csv(path, index=False, mode="w")
+        
+        log.debug(
+            f"Finished writing {data_df.shape[0]} rows to {path} in {perf_counter()-start:0.2f} seconds."
+        )
