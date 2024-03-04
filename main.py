a0ee4688bfab11df80cef69dd661f266b8fd995e
ghaefner <guillaume-haefner@gmx.de>
2024-03-04 16:55:11 +0100

    Example main file

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from pytc.src.model import Model
 from pytc.src.reader import UnifyReader
 from pytc.src.writer import CsvWriter
+from pytc.config import Config, ColumnMap
 
 
 test = Model(
     name="ULD",
     name_long = "Unify Liquid Data",
-    reader = UnifyReader
-)
+    reader = UnifyReader(config=Config),
+    writer = CsvWriter(file_path="./test.csv"),
+    tasks = [],
+    column_map = ColumnMap.LD
+).run()
