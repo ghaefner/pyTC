c2f1b08a1525a3fc4408c5318543c162f0584a41
ghaefner <guillaume-haefner@gmx.de>
2024-03-05 14:54:43 +0100

    Edit main.py for debugging

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from pytc.src.model import Model
 from pytc.src.reader import UnifyReader
 from pytc.src.writer import CsvWriter
-from pytc.config import Config, ColumnMap
+from pytc.config import Config, Table, ColumnMap
+from pytc.src.model import Task, dummy
 
 
+print("Here.")
 test = Model(
     name="ULD",
     name_long = "Unify Liquid Data",
     reader = UnifyReader(config=Config),
     writer = CsvWriter(file_path="./test.csv"),
-    tasks = [],
+    tasks = [ Task("", dummy, Table.TRANSACTION) ],
     column_map = ColumnMap.LD
-).run()
+)
+
+print(test.reader.file_path)
+print("Here.")
+test.run()
+print("Here.")
