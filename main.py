0471ee6082dba9f4df30a73e93db7c5837c4c96c
ghaefner <guillaume-haefner@gmx.de>
2024-03-13 20:31:02 +0100

    Fix UnifyReader error in main model class

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from pytc.src.reader import UnifyReader, PTRReader
 from pytc.src.writer import CsvWriter
 from pytc.config import Config, Table
 from pytc.src.model import Task, calculate_share
+from pytc.src.util import convert_value_to_num
 
 
 test = Model(
     name="ULD",
     name_long = "Unify Liquid Data",
-    reader = PTRReader(config=Config),
+    reader = UnifyReader(config=Config),
     writer = CsvWriter(config=Config),
     tasks = [ 
-        Task("", replace_na_value, Table.TRANSACTION),
+        Task("", convert_value_to_num, Table.TRANSACTION),
         Task("", calculate_share, Table.TRANSACTION) 
         ],
 ).run()
