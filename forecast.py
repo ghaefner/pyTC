e0b69a02a22989b4c92114f93211e60a34c809ac
ghaefner <guillaume-haefner@gmx.de>
2024-03-26 17:51:57 +0100

    Add multiple target regions

--- forecast.py
+++ forecast.py
@@ -11,7 +11,7 @@ df = pd.read_csv("pytc/output/facts.csv")
 # df = filter_complete_time_series(df)
 
 TARGET_PRODUCT = "TAXOFIT"
-TARGET_REGION = "NRW"
+TARGET_REGION = ["NRW", "BAY"]
 N_MAX = 2
 N_MIN = 2
 TEST_PERIOD = [datetime(2024, 11, 1), datetime(2024, 12, 23)]
@@ -19,25 +19,27 @@ TEST_PERIOD = [datetime(2024, 11, 1), datetime(2024, 12, 23)]
 df['date'] = pd.to_datetime(df['date'])
 df['period'] = np.select([df['date'] < min(TEST_PERIOD), df['date'] > max(TEST_PERIOD)], ['Pre', 'Post'], default='Test')
 
-filtered_data = df[(df['product'] == TARGET_PRODUCT) & (df['period']=='Pre')]
 
-grouped_data = filtered_data.groupby(['market', 'metric'])
+for tgt_region in TARGET_REGION:
+    filtered_data = df[(df['product'] == TARGET_PRODUCT) & (df['period']=='Pre')]
 
-for group, group_data in grouped_data:
-    market, metric = group
-    cols = ['date', 'region', 'value']
+    grouped_data = filtered_data.groupby(['market', 'metric'])
+
+    for group, group_data in grouped_data:
+        market, metric = group
+        cols = ['date', 'region', 'value']
     
-    df = group_data[cols].pivot_table(index='date', columns='region', values='value', aggfunc='first').reset_index()
+        df = group_data[cols].pivot_table(index='date', columns='region', values='value', aggfunc='first').reset_index()
     
-    complete_regions = group_data['region'].value_counts()[group_data['region'].value_counts() >= len(group_data['date'].unique())].index.tolist()
+        complete_regions = group_data['region'].value_counts()[group_data['region'].value_counts() >= len(group_data['date'].unique())].index.tolist()
 
-    for n in range(N_MIN,N_MAX+1):
-        region_combinations = [comb for comb in combinations(complete_regions, n)]
+        for n in range(N_MIN,N_MAX+1):
+            region_combinations = [comb for comb in combinations(complete_regions, n)]
 
-        for region_comb in region_combinations:
-            # Perform linear regression
-            X = df[list(region_comb)].values.reshape(-1, len(region_comb))
-            y = df[TARGET_REGION].values
+            for region_comb in region_combinations:
+                # Perform linear regression
+                X = df[list(region_comb)].values.reshape(-1, len(region_comb))
+                y = df[tgt_region].values
 
-            res = linear_regression_results(X,y,region_comb)
-            print(res)
+                res = linear_regression_results(X,y,region_comb)
+                print(res)
