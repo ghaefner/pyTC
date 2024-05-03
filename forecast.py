2445e1a7d734583d10307f8c83f2a0ea1a448c71
ghaefner <guillaume-haefner@gmx.de>
2024-05-03 10:38:32 +0200

    Clean up forecasting

--- forecast.py
+++ forecast.py
@@ -1,13 +1,10 @@
 import pandas as pd
-from itertools import combinations
-import numpy as np
-from pytc.config import Columns
-from pytc.src.util import filter_complete_time_series
+from pytc.src.util import filter_complete_timeseries
 from pytc.src.model import run_linear_model
-from datetime import datetime
 
 df = pd.read_csv("pytc/output/facts.csv")
 
+df = filter_complete_timeseries(df)
 res = run_linear_model(df=df)
 
 print(res.head())
